3d7ef5b75806625171f8c207a6854aad038e45a3
sukumarae5 <sukumar.ae5@gmail.com>
2024-04-06 18:38:10 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,7 @@ const express = require("express");
 var app = express();
 const cors = require("cors");
 const connection = require("./connection");
-const multer = require("multer");
-const upload = multer({ dest: "uploads/" }); // Set the destination folder for uploaded images
+
 
 const usernameRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
@@ -86,124 +85,63 @@ app.post("/login", async (req, res) => {
 });
 
 //Register a new user
-// app.post("/register", async (req, res) => {
-//   var data = req.body;
-//   var email = data.email;
-//   if (!usernameRegex.test(email)) {
-//     res.send({
-//       message: "Enter valid email",
-//     });
-//   } else {
-//     if (data.password === data.confirmpassword) {
-//       try {
-//         const [existingUser] = await connection.query(
-//           "SELECT * FROM user_list WHERE email=?",
-//           [email]
-//         );
-//         if (existingUser.length > 0) {
-//           res.send({
-//             message: "user already exist",
-//           });
-//         } else {
-//           var userData = [
-//             data.firstName,
-//             data.lastName,
-//             data.maidenName,
-//             data.age,
-//             data.gender,
-//             data.email,
-//             data.phone,
-//             data.username,
-//             data.password,
-//             data.birthDate,
-//             data.image,
-//             JSON.stringify(data.address),
-//             JSON.stringify(data.bank),
-//           ];
-
-//           await connection.query(
-//             "INSERT INTO user_list(firstName, lastName, maidenName, age, gender, email, phone, username, password, birthDate, image, address, bank) VALUES(?)",
-//             [userData]
-//           );
-//           res.send({
-//             message: "Registered successfully",
-//           });
-//         }
-//       } catch (e) {
-//         console.log(e);
-//         res.send({
-//           message: "All feilds required",
-//         });
-//       }
-//     } else {
-//       res.send({
-//         message: "Password dosen't match",
-//       });
-//     }
-//   }
-// });
-
-// Register using image
-
-
-app.post("/register", upload.single("profileImage"), async (req, res) => {
+app.post("/register", async (req, res) => {
   var data = req.body;
   var email = data.email;
-
-  // Check if image was uploaded
-  if (!req.file) {
-    return res.send({
-      message: "Image not uploaded",
+  if (!usernameRegex.test(email)) {
+    res.send({
+      message: "Enter valid email",
     });
-  }
+  } else {
+    if (data.password === data.confirmpassword) {
+      try {
+        const [existingUser] = await connection.query(
+          "SELECT * FROM user_list WHERE email=?",
+          [email]
+        );
+        if (existingUser.length > 0) {
+          res.send({
+            message: "user already exist",
+          });
+        } else {
+          var userData = [
+            data.firstName,
+            data.lastName,
+            data.maidenName,
+            data.age,
+            data.gender,
+            data.email,
+            data.phone,
+            data.username,
+            data.password,
+            data.birthDate,
+            data.image,
+            JSON.stringify(data.address),
+            JSON.stringify(data.bank),
+          ];
 
-  // Convert the uploaded image to a buffer
-  var imageBuffer = req.file.buffer;
-
-  // Continue with your existing logic to insert data into the database, including the image buffer
-  // ...
-
-  try {
-    const [existingUser] = await connection.query(
-      "SELECT * FROM user_list WHERE email=?",
-      [email]
-    );
-    if (existingUser.length > 0) {
-      res.send({
-        message: "user already exist",
-      });
+          await connection.query(
+            "INSERT INTO user_list(firstName, lastName, maidenName, age, gender, email, phone, username, password, birthDate, image, address, bank) VALUES(?)",
+            [userData]
+          );
+          res.send({
+            message: "Registered successfully",
+          });
+        }
+      } catch (e) {
+        console.log(e);
+        res.send({
+          message: "All feilds required",
+        });
+      }
     } else {
-      var userData = [
-        data.firstName,
-        data.lastName,
-        data.maidenName,
-        data.age,
-        data.gender,
-        data.email,
-        data.phone,
-        data.username,
-        data.password,
-        data.birthDate,
-        imageBuffer, // Insert the image buffer into the database
-        JSON.stringify(data.address),
-        JSON.stringify(data.bank),
-      ];
-
-      await connection.query(
-        "INSERT INTO user_list(firstName, lastName, maidenName, age, gender, email, phone, username, password, birthDate, profileImage, address, bank) VALUES(?)",
-        [userData]
-      );
       res.send({
-        message: "Registered successfully",
+        message: "Password dosen't match",
       });
     }
-  } catch (e) {
-    console.log(e);
-    res.send({
-      message: "All fields required",
-    });
   }
 });
+
 
 //get products
 app.get("/products", async (req, res) => {
