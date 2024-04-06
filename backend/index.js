ba21e8df816c7d96ad7b2dee97e7f72b8df5a6a5
sukumarae5 <sukumar.ae5@gmail.com>
2024-04-06 09:56:50 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send({ message: "Api Working Fine" });
+  res.send({ message: "Api is Working Fine" });
 });
 
 app.get("/users", async (req, res) => {
