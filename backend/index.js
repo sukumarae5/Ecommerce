f98734353c2c5113e4e44e12000126de103e4ccb
sukumarae5 <sukumar.ae5@gmail.com>
2024-04-09 19:35:48 +0530

    initial message

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,14 @@ const connection = require("./connection");
 const usernameRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 app.use(express.json());
-app.use(cors());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 
 app.get("/", (req, res) => {
   res.send({ message: "Api is Working Fine" });
