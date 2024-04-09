41dc56de13c5fb1b54e877063f6ea63f73928d4d
sukumarae5 <sukumar.ae5@gmail.com>
2024-04-09 19:42:03 +0530

    initial message

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const connection = require("./connection");
 const usernameRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 app.use(express.json());
+app.use(cors());
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
