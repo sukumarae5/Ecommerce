8d93b1a33718113a3a7f5b90fae5aa851ae9a6a8
sukumarae5 <163891820+sukumarae5@users.noreply.github.com>
2024-04-11 09:53:50 +0530

    Update connection.js

--- backend/connection.js
+++ backend/connection.js
@@ -1,7 +1,7 @@
 const myqsl = require("mysql2/promise");
 var sqlConnection = myqsl.createPool({
   host: "0.tcp.in.ngrok.io",
-  port: 13869,
+  port: 10234,
   user: "root",
   password: "123456",
   database: "ecommercedb",
