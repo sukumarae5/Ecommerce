9856a2212b4daac4ee0976ad3f8fc911af75eaa7
sukumarae5 <163891820+sukumarae5@users.noreply.github.com>
2024-04-24 19:35:37 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,9 @@ const connection = require("./connection");
 const usernameRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin:'http://localhost:3000  '
+}));
 
 app.get("/", (req, res) => {
   try {
